d7528cac7ee40defdb18900493e2175fea13e504
Marco Ricci <mr.ricci79@gmail.com>
2023-06-07 08:59:28 +0100

    geocoder test1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,9 @@ class PagesController < ApplicationController
   def home
     @header = ["Find your Happy", "Search properties for sale and to rent in the UK"]
     @city = request.location.city
+    @country= request.location.country
+    @longitude = request.location.longitude
+    @latitude = request.location.latitude
   end
 
   def dashboard
